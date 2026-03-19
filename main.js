25097a12b2ffd072af4f1196fa87e33d4c29eb76
SungYang Wu <david.wu@zenplustek.com>
2026-03-19 09:47:03 +0800

    Check

--- main.js
+++ main.js
@@ -357,6 +357,7 @@ console.log("Window inner Height: ",window.innerHeight);
 console.log("Window Height: ",(window.innerHeight*window.devicePixelRatio/ppi)*0.0254);
 console.log("Window Ratio: ",window.devicePixelRatio);
 console.log("GL Width/Height: ", gl.drawingBufferWidth, gl.drawingBufferHeight);
+console.log("Canvas Width/Height: ", canvas.width, canvas.height);
   const viewport = gl.getParameter(gl.VIEWPORT);
   console.log('Viewport:', viewport); // [x, y, width, height]
 
